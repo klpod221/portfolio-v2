e597faa9f0da50b6b4dcb0e20e6b50bc28ca7534
klpod221 <klpod221@gmail.com>
2023-08-24 01:53:37 +0700

    update animation

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -12,21 +12,21 @@ import { FaQuoteLeft } from "react-icons/fa";
 // testimonial data
 const testimonialData = [
   {
-    image: "/t-avt-1.png",
+    image: "/images/t-avt-1.png",
     name: "Anne Smith",
     position: "Customer",
     message:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: "/t-avt-2.png",
+    image: "/images/t-avt-2.png",
     name: "Jane Doe",
     position: "Customer",
     message:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: "/t-avt-3.png",
+    image: "/images/t-avt-3.png",
     name: "Jhon Doe",
     position: "Customer",
     message:
--- components/Transition.js
+++ components/Transition.js
@@ -1,17 +1,18 @@
+import Image from "next/image";
 import { motion } from "framer-motion";
 
 const transitionVariants = {
   initial: {
-    x: '100%',
-    width: '100%'
+    x: "100%",
+    width: "100%",
   },
   animate: {
-    x: '0%',
-    width: '0%',
+    x: "0%",
+    width: "0%",
   },
   exit: {
-    x: ['0%', '100%'],
-    width: ['0%', '100%'],
+    x: ["0%", "100%"],
+    width: ["0%", "100%"],
   },
 };
 
@@ -24,15 +25,25 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.2, duration: 0.6, ease: 'easeInOut' }}
-      />
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+      >
+        {/* loading spinner */}
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <Image
+            src="/images/loading.svg"
+            width={150}
+            height={150}
+            alt="loading spinner"
+          />
+        </div>
+      </motion.div>
       <motion.div
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]"
         variants={transitionVariants}
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.4, duration: 0.6, ease: 'easeInOut' }}
+        transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
       />
       <motion.div
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
@@ -40,7 +51,7 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.6, duration: 0.6, ease: 'easeInOut' }}
+        transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
       />
     </>
   );
