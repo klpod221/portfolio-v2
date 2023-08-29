81452ac18d71bba4efaf0dbf95d238fd230a1402
klpod221 <klpod221@gmail.com>
2023-08-29 18:11:13 +0700

    update start page

--- pages/projects/startpage/index.js
+++ pages/projects/startpage/index.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Head from "next/head";
 import AnalogueClock from "../../../components/startpage/AnalogueClock";
 import RandomBackground from "../../../components/RandomBackground";
 
@@ -34,29 +35,34 @@ const quickLinks = [
 
 const StartPage = () => {
   return (
-    <RandomBackground>
-      <div className="flex flex-col items-center justify-center h-screen">
-        {/* analogue clock */}
-        <AnalogueClock />
+    <>
+      <Head>
+        <title>Start Page | klpod221</title>
+      </Head>
+      <RandomBackground>
+        <div className="flex flex-col items-center justify-center h-screen">
+          {/* analogue clock */}
+          <AnalogueClock />
 
-        {/* quick link */}
-        <div className="flex items-center gap-x-10 text-3xl mt-6">
-          {quickLinks.map((link, index) => {
-            return (
-              <Link
-                href={link.url}
-                key={index}
-                className="text-white hover:text-accent transition-all duration-300 shadow-md rounded-full"
-                title={link.name}
-                target="_blank"
-              >
-                {link.icon}
-              </Link>
-            );
-          })}
+          {/* quick link */}
+          <div className="flex items-center gap-x-10 text-3xl mt-6">
+            {quickLinks.map((link, index) => {
+              return (
+                <Link
+                  href={link.url}
+                  key={index}
+                  className="text-white hover:text-accent transition-all duration-300 shadow-md rounded-full"
+                  title={link.name}
+                  target="_blank"
+                >
+                  {link.icon}
+                </Link>
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </RandomBackground>
+      </RandomBackground>
+    </>
   );
 };
 
