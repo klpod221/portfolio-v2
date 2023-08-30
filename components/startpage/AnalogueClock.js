2617cc052638257d6acc8e67eb6dae424edb3b4b
klpod221 <klpod221@gmail.com>
2023-08-30 12:56:54 +0700

    fix analogue clock

--- components/startpage/AnalogueClock.js
+++ components/startpage/AnalogueClock.js
@@ -2,34 +2,29 @@ import { useState, useEffect } from "react";
 
 const AnalogueClock = () => {
   const [time, setTime] = useState(new Date());
+  const [hoursStyle, setHoursStyle] = useState({});
+  const [minutesStyle, setMinutesStyle] = useState({});
+  const [secondsStyle, setSecondsStyle] = useState({});
 
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(new Date());
+      const hours = time.getHours();
+      const minutes = time.getMinutes();
+      const seconds = time.getSeconds();
+
+      const hoursDegreesCalc = hours * 30 + minutes * 0.5;
+      const minutesDegreesCalc = minutes * 6 + seconds * 0.1;
+      const secondsDegreesCalc = seconds * 6;
+
+      setHoursStyle({ transform: `rotate(${hoursDegreesCalc}deg)` });
+      setMinutesStyle({ transform: `rotate(${minutesDegreesCalc}deg)` });
+      setSecondsStyle({ transform: `rotate(${secondsDegreesCalc}deg)` });
     }, 1000);
 
     return () => clearInterval(interval);
   }, [time]);
 
-  const hours = time.getHours();
-  const minutes = time.getMinutes();
-  const seconds = time.getSeconds();
-
-  const hoursDegrees = hours * 30 + minutes * 0.5;
-  const minutesDegrees = minutes * 6 + seconds * 0.1;
-  const secondsDegrees = seconds * 6;
-
-  const hoursStyle = {
-    transform: `rotate(${hoursDegrees}deg)`,
-  };
-
-  const minutesStyle = {
-    transform: `rotate(${minutesDegrees}deg)`,
-  };
-
-  const secondsStyle = {
-    transform: `rotate(${secondsDegrees}deg)`,
-  };
 
   return (
     <svg className="max-w-[15em] w-full h-auto" viewBox="0 0 100 100" filter="drop-shadow(0 0 0.5rem rgba(0,0,0,0.25))">
